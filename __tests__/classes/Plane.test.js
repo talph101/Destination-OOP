765f9d298f39c80fcbc580245c104ca613bdec78
Tiffany <139176913+talph101@users.noreply.github.com>
2024-09-12 14:54:04 -0500

    Tests complete for Plane class.

--- __tests__/classes/Plane.test.js
+++ __tests__/classes/Plane.test.js
@@ -3,21 +3,21 @@ const Plane = require('../../classes/Plane')
 const { describe, expect, it, test } =  require('@jest/globals')
 
 describe("Tests for Plane class.", () => {
-    const instance = new Plane("Boieng", "Jamaica");
+    const instance = new Plane("Boeing", "Jamaica");
     test("company, origin, and destination are assigned to the correct value", () => {
-        expects(instance.company).toBe("Boeing");
-        expects(instance.origin.length).toBe(3);
-        expects(instance.destination).toBe("Jamaica");
+        expect(instance.company).toBe("Boeing");
+        expect(instance.origin.length).toBe(3);
+        expect(instance.destination).toBe("Jamaica");
     })
 
     test("passengers array initializes as an empty array", () => {
-        expects(instance.passengers).toEqual([]);
+        expect(instance.getPassengers()).toEqual([]);
     })
 
     test("add a Person to the passengers array using addPassenger", () => {
         const person = new Person("henry", "london");
 
         instance.addPassenger(person);
-        expect(instance.addPassenger).toEqual([person]);
+        expect(instance.getPassengers()).toEqual([person]);
     })
 })
--- classes/Airport.js
+++ classes/Airport.js
@@ -1,4 +1,5 @@
 class Airport {
+    // static aiportCode = "JFK";
 
 
 }
